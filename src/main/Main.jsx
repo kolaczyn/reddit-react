de74ef8a2f17f43d12e39f144423696278cfa3d9
kolaczyn <kolaczyn@protonmail.com>
2020-07-19 01:00:44 +0200

    changed .js files to .jsx to make emmet work

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SortBar from "./SortBar"
+import PostsTimeline from "./PostsTimeline";
 
 export default function Main() {
   return (
       <main className="main-section">
         <SortBar/>
+        <PostsTimeline/>
       </main>
   );
 }
---
+++ src/main/PostsTimeline.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from '../posts/Post'
+
+export default function PostsTimeline() {
+  return (
+    <div>
+      <Post/>
+      <Post/>
+      <Post/>
+      <Post/>
+    </div>
+  )
+}
